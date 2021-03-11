9f5c3b9485853806423a383fac9b14bbb907c1c5
Alvaro Lopez Garcia <lopezalvaro0@gmail.com>
2021-03-11 20:33:56 +0100

    act

--- app/Http/Middleware/isAdmin.php
+++ app/Http/Middleware/isAdmin.php
@@ -6,20 +6,3 @@ use Closure;
 use Auth;
 use Illuminate\Http\Request;
 
-class IsAdmin
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle(Request $request, Closure $next)
-    {
-        if(Auth::user() && Auth::user()->rol == 1){
-            return $next($request);
-        }
-        return redirect('home')->with('error','No puedes acceder ahí');
-    }
-}
