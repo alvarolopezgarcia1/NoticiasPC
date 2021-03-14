87250cd82b87e0b63a49ddc7d5f63f71952b3309
Alvaro Lopez Garcia <lopezalvaro0@gmail.com>
2021-03-14 15:20:38 +0100

    actualizacion

--- resources/views/curso/cursoDetalles.blade.php
+++ resources/views/curso/cursoDetalles.blade.php
@@ -8,14 +8,6 @@
 </div>
 @endif
 <div class="contenido" style="margin : 5vh 10vh 10vh 10vh;background-color: rgba(255, 255, 255, 0.8);">
-    <div class="row" style="margin : 3vh;">
-        <button href="/" class="btn btn-primary btn1 " style="margin : 3vh;float:left ;">Todos los cursos
-            <a href="/showCursos" data-toggle="tooltip" data-placement="top" title="Editar" style="color : white;">
-                    <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm9.5 8.5a.5.5 0 0 0 0-1H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5z"/>
-                </svg>
-            </a>
-        </button>
-    </div>
     <div>
        <div id="card3" class="card mb-3">
         <img class="card-img-top cardImg2" src="{{$curso->foto}}" alt="Card image cap">
